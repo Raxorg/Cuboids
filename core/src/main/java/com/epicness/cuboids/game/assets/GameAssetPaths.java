155a1b97c497e5f6be4b6ed5318be406f6d9468f
Groxar <lrdolucho@gmail.com>
2024-06-22 14:26:18 -0400

    Sounds assets added

--- core/src/main/java/com/epicness/cuboids/game/assets/GameAssetPaths.java
+++ core/src/main/java/com/epicness/cuboids/game/assets/GameAssetPaths.java
@@ -1,16 +1,38 @@
 package com.epicness.cuboids.game.assets;
 
 import com.badlogic.gdx.assets.AssetDescriptor;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-
+import com.badlogic.gdx.audio.Sound;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameAssetPaths {
-
     static final List<AssetDescriptor<?>> ASSETS;
+
+    public static final AssetDescriptor<Sound> DO1_SOUND;
+
+    public static final AssetDescriptor<Sound> DO2_SOUND;
+
+    public static final AssetDescriptor<Sound> FA_SOUND;
+
+    public static final AssetDescriptor<Sound> LA_SOUND;
+
+    public static final AssetDescriptor<Sound> MI_SOUND;
+
+    public static final AssetDescriptor<Sound> RE_SOUND;
+
+    public static final AssetDescriptor<Sound> SI_SOUND;
+
+    public static final AssetDescriptor<Sound> SOL_SOUND;
 
     static {
         ASSETS = new ArrayList<>();
+        ASSETS.add(DO1_SOUND = new AssetDescriptor<>("cuboids/game/audios/sounds/do1.swav", Sound.class));
+        ASSETS.add(DO2_SOUND = new AssetDescriptor<>("cuboids/game/audios/sounds/do2.swav", Sound.class));
+        ASSETS.add(FA_SOUND = new AssetDescriptor<>("cuboids/game/audios/sounds/fa.swav", Sound.class));
+        ASSETS.add(LA_SOUND = new AssetDescriptor<>("cuboids/game/audios/sounds/la.swav", Sound.class));
+        ASSETS.add(MI_SOUND = new AssetDescriptor<>("cuboids/game/audios/sounds/mi.swav", Sound.class));
+        ASSETS.add(RE_SOUND = new AssetDescriptor<>("cuboids/game/audios/sounds/re.swav", Sound.class));
+        ASSETS.add(SI_SOUND = new AssetDescriptor<>("cuboids/game/audios/sounds/si.swav", Sound.class));
+        ASSETS.add(SOL_SOUND = new AssetDescriptor<>("cuboids/game/audios/sounds/sol.swav", Sound.class));
     }
 }
--- core/src/main/java/com/epicness/cuboids/game/assets/GameAssets.java
+++ core/src/main/java/com/epicness/cuboids/game/assets/GameAssets.java
@@ -1,17 +1,72 @@
 package com.epicness.cuboids.game.assets;
 
-import static com.epicness.cuboids.game.assets.GameAssetPaths.ASSETS;
+import static com.epicness.cuboids.game.assets.GameAssetPaths.*;
 
+import com.badlogic.gdx.audio.Sound;
 import com.epicness.fundamentals.assets.Assets;
 
 public class GameAssets extends Assets {
+    private Sound do1;
+
+    private Sound do2;
+
+    private Sound fa;
+
+    private Sound la;
+
+    private Sound mi;
+
+    private Sound re;
+
+    private Sound si;
+
+    private Sound sol;
 
     public GameAssets() {
         super(ASSETS);
     }
 
     @Override
-    protected void initializeAssets() {
+    public void initializeAssets() {
+        do1 = get(DO1_SOUND);
+        do2 = get(DO2_SOUND);
+        fa = get(FA_SOUND);
+        la = get(LA_SOUND);
+        mi = get(MI_SOUND);
+        re = get(RE_SOUND);
+        si = get(SI_SOUND);
+        sol = get(SOL_SOUND);
+    }
 
+    public Sound getDo1() {
+        return do1;
+    }
+
+    public Sound getDo2() {
+        return do2;
+    }
+
+    public Sound getFa() {
+        return fa;
+    }
+
+    public Sound getLa() {
+        return la;
+    }
+
+    public Sound getMi() {
+        return mi;
+    }
+
+    public Sound getRe() {
+        return re;
+    }
+
+    public Sound getSi() {
+        return si;
+    }
+
+    public Sound getSol() {
+        return sol;
     }
 }
