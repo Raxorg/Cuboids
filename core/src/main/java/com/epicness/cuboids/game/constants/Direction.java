a3798288d4bccdbb7d80422584579c5eab6abc5e
Groxar <lrdolucho@gmail.com>
2024-07-10 20:58:27 -0400

    Direction enum

--- core/src/main/java/com/epicness/cuboids/game/constants/Direction.java
+++ core/src/main/java/com/epicness/cuboids/game/constants/Direction.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.math.Vector2;
 
 public enum Direction {
 
-    UP(0f, 1f), DOWN(0f, -1f), LEFT(-1f, 0f), RIGHT(1f, 0f), UP_RIGHT(1f, 1f), UP_LEFT(-1f, 1f), DOWN_RIGHT(1f, -1f), DOWN_LEFT(-1f, -1f);
+    UP(0f, 1f), DOWN(0f, -1f), LEFT(-1f, 0f), RIGHT(1f, 0f),
+    UP_RIGHT(1.02f, 1.02f), UP_LEFT(-1.02f, 1.02f), DOWN_RIGHT(1.02f, -1.02f), DOWN_LEFT(-1.02f, -1.02f);
 
+    public static final Direction DO = DOWN, RE = DOWN_LEFT, MI = LEFT, FA = UP_LEFT, SOL = UP, LA = UP_RIGHT, SI = RIGHT, DO2 = DOWN_RIGHT;
     public final Vector2 vector;
 
     Direction(float x, float y) {
