fd6912239f206bcca578f136fd74689f96a6c916
Groxar <lrdolucho@gmail.com>
2024-07-10 21:00:19 -0400

    Life handling

--- core/src/main/java/com/epicness/cuboids/game/logic/player/LifeHandler.java
+++ core/src/main/java/com/epicness/cuboids/game/logic/player/LifeHandler.java
@@ -7,15 +7,24 @@ import static com.epicness.fundamentals.constants.SharedConstants.CAMERA_HEIGHT;
 
 import com.badlogic.gdx.utils.SnapshotArray;
 import com.epicness.cuboids.game.logic.GameLogicHandler;
+import com.epicness.cuboids.game.logic.enemies.WaveHandler;
+import com.epicness.cuboids.game.stuff.bidimensional.Enemy;
 import com.epicness.fundamentals.stuff.shapes.bidimensional.Circle;
 
 public class LifeHandler extends GameLogicHandler {
 
     private SnapshotArray<Circle> lives;
+    private SnapshotArray<Enemy> enemies;
 
     @Override
     protected void init() {
         lives = stuff.getLives();
+        enemies = stuff.getWorld2D().getEnemies();
+
+        refillLives();
+    }
+
+    public void refillLives() {
         lives.clear();
         for (int i = 0; i < INITIAL_LIVES; i++) {
             lives.add(new Circle(10f + LIFE_SIZE * i + 10f * i, CAMERA_HEIGHT - LIFE_SIZE - 10f, LIFE_RADIUS));
@@ -24,8 +33,13 @@ public class LifeHandler extends GameLogicHandler {
 
     public void loseLife() {
         lives.removeIndex(lives.size - 1);
+
         if (lives.isEmpty()) {
-            // todo: restart wave
+            enemies.begin();
+            enemies.clear();
+            enemies.end();
+            refillLives();
+            get(WaveHandler.class).retryWave();
         }
     }
 }
