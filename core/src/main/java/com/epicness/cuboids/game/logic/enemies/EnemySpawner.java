dbbaf951a480e1a4e4849b03d051b12e13508ed5
Groxar <lrdolucho@gmail.com>
2024-07-10 20:39:41 -0400

    Sound effects

--- core/src/main/java/com/epicness/cuboids/game/logic/enemies/EnemySpawner.java
+++ core/src/main/java/com/epicness/cuboids/game/logic/enemies/EnemySpawner.java
@@ -3,7 +3,6 @@ package com.epicness.cuboids.game.logic.enemies;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.SnapshotArray;
 import com.epicness.cuboids.game.logic.GameLogicHandler;
-import com.epicness.cuboids.game.logic.other.SoundHandler;
 import com.epicness.cuboids.game.stuff.bidimensional.Base;
 import com.epicness.cuboids.game.stuff.bidimensional.Enemy;
 import com.epicness.cuboids.game.stuff.bidimensional.EnemySpawn;
@@ -27,6 +26,5 @@ public class EnemySpawner extends GameLogicHandler {
         enemy.setPosition(spawn.getCenter(aux1));
         enemy.distanceToCenter = enemy.getCenter(aux1).dst(base.getCenter(aux2));
         enemies.add(enemy);
-        get(SoundHandler.class).getSound(enemy).play();
     }
 }
